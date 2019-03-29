e812836bab8452f168d03530de889f25130c1d99
yurikbv <yurikbvv@gmail.com>
2019-03-29 23:32:44 +0300

    first commit

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="rmdb-header">
+        <div className="rmdb-header-content">
+          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+          <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
