6ae547a16f67daafe4c2050a832f5e7ec133b345
yurikbv <yurikbvv@gmail.com>
2019-04-02 14:19:13 +0300

    deploy

--- src/components/elements/Buttons.js
+++ src/components/elements/Buttons.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
-import './Buttons.css';
 import {Button, Segment} from "semantic-ui-react";
 
 class Buttons extends Component {
